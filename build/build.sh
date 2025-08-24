ff7e14c6b50cb73fa2a524e3c2a78c18a4f3c1e4
G1 <65892403+AllG1@users.noreply.github.com>
2025-08-24 22:44:29 +0900

    Revert "Release v0.2.1 (#2)" (#3)
    This reverts commit b3dcf7a9327801d9f6b3cb6951eb837a0f7c9557.

--- build/build.sh
+++ build/build.sh
@@ -2,11 +2,7 @@
 source ./build/.env
 # Clear current docker container & image
 docker rm -f python-flask-api-mockup-container
-docker rm -f python-flask-api-mockup-mysql
 docker rmi python-flask-api-mockup-container-img:$VERSION
-docker rmi python-flask-api-mockup-mysql-img:$VERSION
 docker container prune --force --filter label=python-flask-api-mockup-container:$VERSION
-docker container prune --force --filter label=python-flask-api-mockup-mysql:$VERSION
 # Build new docker image
-docker build -t python-flask-api-mockup-container-img:$VERSION -f ./build/app.dockerfile .
-docker build -t python-flask-api-mockup-mysql-img:$VERSION -f ./build/mysql.dockerfile .
+docker build -t python-flask-api-mockup-container-img:$VERSION -f ./build/Dockerfile .
